b4d55aa5260a5ccb5887a68151599eab48eadd6c
ihyo-yoshiaki <ihyo.yoshiaki.nagoyoaml@gmail.com>
2023-07-08 17:36:52 +0900

    chap09-2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,5 @@ Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::delete('/posts/{post}', [PostController::class, 'delete']);
-
+Route::get('/categories/{category}', [CategoryController::class, 'index']);
 
